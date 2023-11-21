58c2d4cfb64a17b378f80cfdf1eacc7cb27040e4
agathe.fernandes <agathefernandes28@gmail.com>
2023-11-21 13:06:20 -0500

    setup file

---
+++ equipy/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="EquiPy",
+    version="0.0.1",
+    author="Agathe F, Suzie G, Francois H, Philipp R",
+    author_email="nemecontactepas@gmail.com",
+    description="A package to get fairness on your predictions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/a19ferna/ssa",
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'scikit-learn',
+                      'matplotlib', 'pandas', 'statsmodels', 'seaborn'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
