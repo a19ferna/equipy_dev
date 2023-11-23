1fa68beb23da8d31c159b86d247dd14f2cd5c90b
agathe.fernandes <agathefernandes28@gmail.com>
2023-11-23 11:05:51 -0500

    problems with init

--- equipy/__init__.py
+++ equipy/__init__.py
@@ -1,3 +1,3 @@
-from fairness import __init__
-from graphs import __init__
-from metrics import __init__
+from equipy.fairness import __init__
+from equipy.graphs import __init__
+from equipy.metrics import __init__
